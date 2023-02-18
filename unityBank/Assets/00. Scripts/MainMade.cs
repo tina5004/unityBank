d65e1067d2c6b5a4420189aec731dae3e84e7e87
냠냠 <tina5004@naver.com>
2023-02-18 15:35:23 +0900

    bankUnity 3

---
+++ unityBank/Assets/00. Scripts/MainMade.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMade : MonoBehaviour
+{
+    //¾ÀÀüÈ¯
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("Bank");
+    }
+}
